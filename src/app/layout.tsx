c007035c680a35e7ef6eccf87111ed69add93712
Kalpak Pimpale <kalpak2404@gmail.com>
2025-06-04 13:00:34 +0000

    "https://fonts.google.com/specimen/WDXL+Lubrifont+TC?preview.text=Dual%20Insights"
    here is the font link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=WDXL+Lubrifont+TC&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
         <InteractiveBackground />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Spectral', 'serif'],
-        headline: ['Spectral', 'serif'],
+        body: ['"WDXL Lubrifont TC"', 'serif'],
+        headline: ['"WDXL Lubrifont TC"', 'serif'],
         code: ['monospace'],
       },
       colors: {
